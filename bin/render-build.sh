267729c11b8eddacf7d62dafcb5809a80d207db6
Seth <seth.bbrendle@gmail.com>
2022-11-23 10:30:25 -0500

    Depoloy success

--- bin/render-build.sh
+++ bin/render-build.sh
@@ -11,3 +11,11 @@ cp -a client/build/. public/
 bundle install
 bundle exec rake db:migrate 
 bundle exec rake db:seed # if you have seed data, run this command for the initial deploy only to avoid duplicate records
+
+# Initial Render.com build commands
+# bundle install; bundle exec rake assets:precompile; bundle exec rake assets:clean;
+# changed to: ./bin/render-build.sh
+
+# Initial Render.com start commands
+# bundle exec puma -t 5:5 -p ${PORT:-3000} -e ${RACK_ENV:-development}
+# changed to: bundle exec puma -C config/puma.rb
